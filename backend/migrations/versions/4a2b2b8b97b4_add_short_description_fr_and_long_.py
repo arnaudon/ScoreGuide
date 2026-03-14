923a71ea304da86650f8facc8b1657e787eefef2
arnaudon <alexis.arnaudon@epfl.ch>
2026-03-14 18:28:08 +0100

    black

--- backend/migrations/versions/4a2b2b8b97b4_add_short_description_fr_and_long_.py
+++ backend/migrations/versions/4a2b2b8b97b4_add_short_description_fr_and_long_.py
@@ -5,25 +5,41 @@ Revises: 6df28f7fbc33
 Create Date: 2024-03-14 00:00:00.000000
 
 """
+
 from typing import Sequence, Union
 
 from alembic import op
 import sqlalchemy as sa
 import sqlmodel
 
-
 # revision identifiers, used by Alembic.
-revision: str = '4a2b2b8b97b4'
-down_revision: Union[str, None] = '6df28f7fbc33'
+revision: str = "4a2b2b8b97b4"
+down_revision: Union[str, None] = "6df28f7fbc33"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
 
 def upgrade() -> None:
-    op.add_column('score', sa.Column('short_description_fr', sqlmodel.sql.sqltypes.AutoString(), nullable=False, server_default=''))
-    op.add_column('score', sa.Column('long_description_fr', sqlmodel.sql.sqltypes.AutoString(), nullable=False, server_default=''))
+    op.add_column(
+        "score",
+        sa.Column(
+            "short_description_fr",
+            sqlmodel.sql.sqltypes.AutoString(),
+            nullable=False,
+            server_default="",
+        ),
+    )
+    op.add_column(
+        "score",
+        sa.Column(
+            "long_description_fr",
+            sqlmodel.sql.sqltypes.AutoString(),
+            nullable=False,
+            server_default="",
+        ),
+    )
 
 
 def downgrade() -> None:
-    op.drop_column('score', 'long_description_fr')
-    op.drop_column('score', 'short_description_fr')
+    op.drop_column("score", "long_description_fr")
+    op.drop_column("score", "short_description_fr")
